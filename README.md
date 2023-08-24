b4827cf4518f0b156fc87d61ea9b49da318e722b
YuriLucena <yurikaicklucena@gmail.com>
2023-08-24 14:08:53 -0300

    criando os scripts de estilização

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Aula-Curso.dev
+
 Projeto para seguir as aulas do Curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return (
-        <>
-            <h1>Bora Lol?</h1>
-            <a href="https://www.youtube.com/shorts/DtnXKXN39XU">Link Suspeito</a>
-        </>
-    )
+  return (
+    <>
+      <h1>Bora Lol?</h1>
+      <a href="https://www.youtube.com/shorts/DtnXKXN39XU">Link Suspeito</a>
+    </>
+  );
 }
 
 export default Home;
